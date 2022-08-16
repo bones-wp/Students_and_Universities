d5aa0519cc76ec063ed013f230e7498fd27a9d31
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-08-16 14:03:11 +0300

    28.7 - начало

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,8 @@ import modelclass.Student;
 import modelclass.University;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import static modelclass.StudyProfile.MATHEMATICS;
-import static modelclass.StudyProfile.PHYSICS;
 
 public class Main {
 
@@ -19,38 +16,38 @@ public class Main {
         Reader.readStudents();
 
         String jSonStudents = JsonUtil.jCollectionStudent();
-        System.out.println(jSonStudents);
+        //System.out.println(jSonStudents);
 
         List<Student> listStudentsFromJson = JsonUtil.studentsFromJson(jSonStudents);
-        System.out.println(listStudentsFromJson);
+        /*System.out.println(listStudentsFromJson);
         if (listStudentsFromJson.size() == Reader.students.size()) {
             System.out.println("\n Сериализация коллекции студентов выполнена корректно \n");
-        }
+        }*/
 
         Reader.readUniversity();
 
         String jSonUniversity = JsonUtil.jCollectionUniversity();
-        System.out.println(jSonUniversity);
+        //System.out.println(jSonUniversity);
 
         List<University> listUniversityFromJson = JsonUtil.universityFromJson(jSonUniversity);
-        System.out.println(listUniversityFromJson);
+        /*System.out.println(listUniversityFromJson);
         if (listUniversityFromJson.size() == Reader.universities.size()) {
             System.out.println("\n Сериализация коллекции университетов выполнена корректно \n");
-        }
+        }*/
 
 
         Reader.students.stream()
                 .sorted(NeedableComparator.getStudentsCompare(EnumStudentsCompare.AVGeXAMsCORE))
                 .forEach(x -> {
-                    System.out.println("\n Сериализованный объект: " + JsonUtil.toJSonStudent(x));
-                    System.out.println("Десериализованный объект: " + JsonUtil.fromJsonStudent(JsonUtil.toJSonStudent(x)));
+                    /*System.out.println("\n Сериализованный объект: " + */JsonUtil.toJSonStudent(x);
+                    /*System.out.println("Десериализованный объект: " + */JsonUtil.fromJsonStudent(JsonUtil.toJSonStudent(x));
                 });
 
         Reader.universities.stream()
                 .sorted(NeedableComparator.getUniversityCompare(EnumUniversityCompare.ID))
                 .forEach(x -> {
-                    System.out.println("\n Сериализованный объект: " + JsonUtil.toJSonUniversity(x));
-                    System.out.println("Десериализованный объект: " + JsonUtil.fromJsonUniversity(JsonUtil.toJSonUniversity(x)));
+                    /*System.out.println("\n Сериализованный объект: " + */JsonUtil.toJSonUniversity(x);
+                    /*System.out.println("Десериализованный объект: " + */JsonUtil.fromJsonUniversity(JsonUtil.toJSonUniversity(x));
                 });
 
 
--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -6,7 +6,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
